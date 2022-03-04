4545ae7df0de34a72bd4bb85a230c099732dc1fb
Maksym Medvediev <aldoran.ua@gmail.com>
2022-03-04 14:31:38 +0200

    fixed routes

--- src/components/Router/index.jsx
+++ src/components/Router/index.jsx
@@ -2,14 +2,14 @@ import React, { useCallback } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Home from "~/pages/Home";
-import ResponsiveRoutesPage from "~/pages/ResponsiveRoutes";
-import PopupsRoutesPage from "~/pages/PopupsRoutes";
-import CustomPromptPage from "~/pages/CustomPrompt";
-import ClosePopupPage from "~/pages/ClosePopup";
-import RestorePreventedRoutePage from "~/pages/RestorePreventedRoute";
-import StepperPage from "~/pages/Stepper";
-import TwoPopupsPage from "~/pages/TwoPopups";
+import Home from "../../pages/Home";
+import ResponsiveRoutesPage from "../../pages/1-ResponsiveRoutes";
+import PopupsRoutesPage from "../../pages/PopupsRoutes";
+import CustomPromptPage from "../../pages/CustomPrompt";
+import ClosePopupPage from "../../pages/ClosePopup";
+import RestorePreventedRoutePage from "../../pages/RestorePreventedRoute";
+import StepperPage from "../../pages/Stepper";
+import TwoPopupsPage from "../../pages/TwoPopups";
 
 import AppBar from "../AppBar";
 import CustomPrompt from "../CustomPrompt";
